be539ba7356c45cda18e95dbdff246b8b6ebb74d
Deepak Louis <deepak.l@zohocorp.com>
2021-06-22 18:11:04 +0530

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
     refinedMoonstone: 4
 }
 
+const elvenGauntletRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 3
+}
+
 console.log(elvenShieldRecipe);
+console.log(elvenGauntletRecipe);
