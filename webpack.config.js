847ad4ab4bd51603217af6ea14abb93b08e167a1
Deepak Louis <deepak.l@zohocorp.com>
2021-06-24 11:14:47 +0530

    add css, sass, postcss and hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 let mode = "development";
+let target = "web"; // for HMR in dev
 
 if (process.env.NODE_ENV == "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
 
     devtool: "source-map",
     
     module: {
-        rules : [
+        rules: [
+            {
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader", // keep it before sass so that sass file doesn't contain any vendor prefixes
+                    "sass-loader"
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -20,7 +34,11 @@ module.exports = {
             }
         ]
     },
+
+    plugins: [new MiniCssExtractPlugin()],
+
     devServer: {
-        contentBase: "./dist"
+        contentBase: "./dist", //to specify which folder to access. or else all folders will be displayed
+        hot: true // Enables HMR. Applies the changes without refreshing the dom
     }
 };
